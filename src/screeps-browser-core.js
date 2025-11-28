6154504804c7aea0852e659b4468a69cc326d98c
Etienne Samson <samson.etienne@gmail.com>
2025-11-28 18:16:42 +0100

    Fix Angular not being ready when registering callbacks

--- src/screeps-browser-core.js
+++ src/screeps-browser-core.js
@@ -1,5 +1,18 @@
 window.DomHelper = window.DomHelper || {};
-(function(DomHelper){
+(function(DomHelper) {
+    function waitForAngular() {
+        return new Promise(resolve => {
+            const check = () => {
+                const injector = angular.element(document.body).injector();
+                if (injector) {
+                    resolve(injector);
+                } else {
+                    setTimeout(check, 20);
+                }
+            };
+            check();
+        });
+    }
     DomHelper.addStyle = function (css) {
         let head = document.head;
         if (!head) return;
@@ -20,62 +33,73 @@ window.DomHelper = window.DomHelper || {};
 
 window.ScreepsAdapter = window.ScreepsAdapter || {};
 (function(ScreepsAdapter) {
+    function waitForAngular() {
+        return new Promise(resolve => {
+            const check = () => {
+                const injector = angular.element(document.body).injector();
+                if (injector) {
+                    resolve(injector);
+                } else {
+                    setTimeout(check, 20);
+                }
+            };
+            check();
+        });
+    }
+
     // Listen for changes to the main screeps view
     // Examples: roomEntered, scriptClick, consoleClick, worldMapEntered, simulationMainMenu, gameLobby
     ScreepsAdapter.onViewChange = function (callback) {
-        let rootScope = angular.element(document.body).scope();
-        if (!rootScope.viewChangeCallbacks) {
-            let tutorial = angular.element(document.body).injector().get("Tutorial");
-            console.log("Overriding Tutorial.trigger");
 
-            // intercept events as they are passed to the tutorial popup manager
-            tutorial._trigger = tutorial.trigger;
-            tutorial.trigger = function(triggerName, unknownB) {
-                for (let i in rootScope.viewChangeCallbacks) {
-                    rootScope.viewChangeCallbacks[i](triggerName);
-                }
-                tutorial._trigger(triggerName, unknownB);
-            };
+        waitForAngular().then(() => {
+            let rootScope = angular.element(document.body).scope();
+            if (!rootScope.viewChangeCallbacks) {
+                let tutorial = angular.element(document.body).injector().get("Tutorial");
+                console.log("Overriding Tutorial.trigger");
 
-            rootScope.viewChangeCallbacks = [];
-        }
+                // intercept events as they are passed to the tutorial popup manager
+                tutorial._trigger = tutorial.trigger;
+                tutorial.trigger = function(triggerName, unknownB) {
+                    for (let i in rootScope.viewChangeCallbacks) {
+                        rootScope.viewChangeCallbacks[i](triggerName);
+                    }
+                    tutorial._trigger(triggerName, unknownB);
+                };
 
-        rootScope.viewChangeCallbacks.push(callback);
+                rootScope.viewChangeCallbacks = [];
+            }
+
+            rootScope.viewChangeCallbacks.push(callback);
+        });
     };
 
     ScreepsAdapter.onHashChange = function (callback) {
-        let rootScope = angular.element(document.body).scope();
-        if (!rootScope.hashChangeCallbacks) {
-            rootScope.$watch(() => window.location.hash, function(newVal, oldVal) {
-                for (let i in rootScope.hashChangeCallbacks) {
-                    rootScope.hashChangeCallbacks[i](window.location.hash);
-                }
-            });
-
-            rootScope.hashChangeCallbacks = [];
-        }
-
-        rootScope.hashChangeCallbacks.push(callback);
+        waitForAngular().then(() => {
+            const rootScope = angular.element(document.body).scope();
+            if (!rootScope.hashChangeCallbacks) {
+                rootScope.$watch(() => window.location.hash, function(newVal, oldVal) {
+                    for (let i in rootScope.hashChangeCallbacks) {
+                        rootScope.hashChangeCallbacks[i](window.location.hash);
+                    }
+                });
+                
+                rootScope.hashChangeCallbacks = [];
+            }
+            
+            rootScope.hashChangeCallbacks.push(callback);
+        });
     };
 
     ScreepsAdapter.onRoomChange = function (callback) {
-        let $routeParams = angular.element(document.body).injector().get("$routeParams");
-        let rootScope = angular.element(document.body).scope();
-        if (!rootScope.roomChangeCallbacks) {
-            ScreepsAdapter.onHashChange((hash) => {
-                let room = $routeParams.room;
-                if (room !== rootScope.lastRoom) {
-                    for (let i in rootScope.roomChangeCallbacks) {
-                        rootScope.roomChangeCallbacks[i](room);
-                    }
-                    rootScope.lastRoom = room;
-                }
-            });
-
-            rootScope.roomChangeCallbacks = [];
-        }
-
-        rootScope.roomChangeCallbacks.push(callback);
+        ScreepsAdapter.onHashChange((hash) => {
+            let rootScope = angular.element(document.body).scope();
+            let $routeParams = angular.element(document.body).injector().get("$routeParams");
+            let room = $routeParams.room;
+            if (room !== rootScope.lastRoom) {
+                callback(room);
+                rootScope.lastRoom = room;
+            }
+        });
     };
 
     // aliases to angular services
